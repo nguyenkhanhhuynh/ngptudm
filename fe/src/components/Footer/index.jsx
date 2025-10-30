90ff971e43edc59c1beca396064798f303388b97
nguyenkhanhhuynh <nguyenkhanhhuynh01@gmail.com>
2025-10-30 23:03:59 +0700

    Initial commit – upload project

---
+++ fe/src/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import './footer.scss';
+
+const Footer = () => {
+    return (
+        <footer>
+            <div>Book Shop | KhanhHuynh</div>
+        </footer>
+    )
+}
+
+export default Footer;
